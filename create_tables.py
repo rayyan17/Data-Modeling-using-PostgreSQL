20c8ad1cf6d618dc5568b20aab7fd5b14ff5cf6a
Rayyan Khalid <rayyankhalid99@gmail.com>
2020-05-12 01:59:20 +0500

    Creating Database to Model Raw Data into Tables

---
+++ create_tables.py
@@ -0,0 +1,29 @@
+"""Script for Creating Database and Tables"""
+
+import psycopg2
+
+
+def create_database():
+    """
+    - Creates and connects to the sparkifydb
+    - Returns the connection and cursor to sparkifydb
+    """
+
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()
+
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    return cur, conn
+
