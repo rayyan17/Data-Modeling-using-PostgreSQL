b36a2341e1061bbf5235adb5698f36e0c921ddd2
Rayyan Khalid <rayyankhalid99@gmail.com>
2020-05-13 00:12:26 +0500

    Insert records to tables queries added

--- sql_queries.py
+++ sql_queries.py
@@ -35,6 +35,32 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time \
                         year int NOT NULL, weekda int NOT NULL);""")
 
 
+# INSERT RECORDS IN TABLES
+
+songplay_table_insert = ("""INSERT INTO songplays \
+                            (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""")
+
+user_table_insert = ("""INSERT INTO users \
+                        (user_id, first_name, last_name, gender, level) \
+                        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level""")
+
+song_table_insert = ("""INSERT INTO songs \
+                        (song_id, title, artist_id, year, duration) \
+                        VALUES (%s, %s, %s, %s, %s)""")
+
+artist_table_insert = ("""INSERT INTO artists \
+                          (artist_id, name, location, latitude, longitude) \
+                          VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) \
+                          DO UPDATE \
+                          SET location=EXCLUDED.location, longitude=EXCLUDED.longitude, latitude=EXCLUDED.latitude""")
+
+
+time_table_insert = ("""INSERT INTO time \
+                        (start_time, hour, day, week, month, year, weekda) \
+                        VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING""")
+
+
 drop_table_queries = [song_table_drop, artist_table_drop, time_table_drop, user_table_drop,
                       songplay_table_drop]
 
