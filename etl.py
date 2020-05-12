75f17c9b452cd6b258495da8a71f576c2e76db9d
Rayyan Khalid <rayyankhalid99@gmail.com>
2020-05-13 00:21:29 +0500

    [ETL] process raw data files

--- etl.py
+++ etl.py
@@ -27,4 +27,25 @@ def process_song_file(cur, filepath):
     cur.execute(artist_table_insert, artist_data)
 
 
+def process_data(cur, conn, filepath, func):
+    """
+    Read all the files from song ang log directory
+    """
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
+
 
